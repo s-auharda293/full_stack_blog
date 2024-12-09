200618bad8b6cc3ef4b025eeb04f8b3da65d199c
Sauharda <077bct036.suharda@sagarmatha.edu.np>
2024-12-09 22:04:32 +0545

    upto 4:25:00

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -3,8 +3,18 @@ import User from "../models/user.model.js";
 import ImageKit from "imagekit";
 
 export const getPosts = async (req, res) => {
-  const posts = await Post.find();
-  res.status(200).json(posts);
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.page) || 2;
+
+  const posts = await Post.find()
+    .populate("user", "username")
+    .limit(limit)
+    .skip((page - 1) * limit);
+
+  const totalPosts = await Post.countDocuments();
+  const hasMore = page * limit < totalPosts;
+
+  res.status(200).json({ posts, hasMore });
 };
 
 export const getPost = async (req, res) => {
